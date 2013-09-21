b0f74dc7bfde0c089cd79266b67c684f83e71973
fr1zle <f@br0tbox.de>
2013-09-21 22:21:25 +0200

    License Headers
    Signed-off-by: fr1zle <f@br0tbox.de>

--- src/main/java/org/spout/platform/controller/PlayController.java
+++ src/main/java/org/spout/platform/controller/PlayController.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2012 Spout LLC <http://www.spout.org>
+ * All Rights Reserved, unless otherwise granted permission.
+ *
+ * You may use and modify for private use, fork the official repository
+ * for contribution purposes, contribute code, and reuse your own code.
+ */
 package org.spout.platform.controller;
 
 import java.io.BufferedReader;
